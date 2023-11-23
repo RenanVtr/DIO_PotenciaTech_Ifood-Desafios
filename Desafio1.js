90c6de3c591723b29b41cbc8d233ed1f185ad35e
RenanVtr <renanvitorg@hotmail.com>
2023-11-23 10:21:39 -0300

    Correcoes de logica

--- Desafio1.js
+++ Desafio1.js
@@ -1,34 +1,44 @@
-let nome 
-let xp
-let nivel
+console.log("Olá aventureiro!");
 
-switch (xp) {
-    case xp<1000:
-        nivel = "Ferro"
+var nome = "Renan";
+var xp = 5000;
+var nivel;
+
+
+switch (true){
+    case (xp <= 1000 ):
+        nivel = "Ferro";
         break;
-    case 1001<xp<2000:
-        nivel = "Bronze"
+
+    case (1001 <= xp && xp <= 2000):
+        nivel = "Bronze";
         break;
-    case 2001<xp<5000:
-        nivel = "Prata"
-        break;    
-    case 5001<xp<7000:
-        nivel = "Ouro"
-        break;    
-    case 7001<xp<8000:
-        nivel = "Platina"
-        break;    
-    case 8001<xp<9000:
-        nivel = "Ascendente"
-        break;    
-    case 9001<xp<10000:
-        nivel = "Imortal"
-        break;    
-    case 10001<xp:
-        nivel = "Radiante"
+
+    case (2001 <= xp && xp <= 5000):
+        nivel = "Prata";
         break;
+   
+    case (5001 <= xp && xp <= 7000):
+        nivel = "Ouro";
+        break;
+  
+    case (7001 <= xp && xp <= 8000):
+        nivel = "Platina";
+        break;
+   
+    case (8001 <= xp && xp <= 9000):
+        nivel = "Ascendente";
+        break;
+   
+    case (9001 <= xp && xp <= 10000):
+        nivel = "Imortal";
+        break;
+  
+    case (10001 <= xp):
+        nivel = "Radiante";
+        break;
+
     default:
         break;
-
-        console.log("O Herói de nome "+ nome+ " está no nível de "+ nivel )
-} 
+}
+    console.log("O Herói de nome "+ nome+ " está no nível de "+ nivel + " com "+ xp+ " de experiência!");
